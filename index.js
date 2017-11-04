54650db79025a6a16f2537cdff9b690f296a8e48
judgejab <mattcleary@gmail.com>
2017-11-04 23:05:44 +0000

    added passing test

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/*In a language of your choice,
+please write a program which accepts as input two arbitrarily nested arrays of strings,
+and returns the sum of the number of necessary one character substitutions
+multiplied by 2 to the power of the depth of nesting at which substitution must occur*/
+
+/* For example:
+
+yourFunction(
+["I", ["love"], [[["awesome", "shiny"]]], "drones"],
+["I", ["hate"], [[["magenta", "dirty"]]], "drains"]
+)
+should return 2(3) + 8(7 + 4) + 3 = 97
+*/
+
 const myFunction = () => {
   return 1;
 };
--- test.js
+++ test.js
@@ -2,6 +2,13 @@ const should = require('chai').should();
 const myFunction = require('./index');
 
 describe('myFunction', () => {
+  it('returns the sum of the number of necessary one character substitutions multiplied by 2 to the power of the depth of nesting at which substitution must occur', () => {
+    myFunction(
+      ['I', ['love'], [[['awesome', 'shiny']]], 'drones'],
+      ['I', ['hate'], [[['magenta', 'dirty']]], 'drains']
+    ).should.equal(97);
+  });
+
   it('If the levels of nesting or the lengths of the strings do not match, return -1', () => {
     myFunction(
       ['I', ['love'], [['awesome']], 'drones'],
