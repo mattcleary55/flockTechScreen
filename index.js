478057bef687071b998c4902ea0eac612e46bdb4
judgejab <mattcleary@gmail.com>
2017-11-06 00:56:37 +0000

    got rid of needless length check

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ const myFunction = (arr1, arr2) => {
 };
 
 const compareStrings = (str1, str2) => {
-  if (str1.length !== str2.length) return -1;
   let diffCharCount = 0;
   for (let i = 0; i < str1.length; i++) if (str1[i] !== str2[i]) diffCharCount++;
   return diffCharCount;
