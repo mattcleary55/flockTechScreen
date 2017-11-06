e0ab67c3b16bb5951d5cb5b087495b26bcb5c3ec
judgejab <mattcleary@gmail.com>
2017-11-06 00:51:01 +0000

    refactored and added comments

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
+/*
+  This program which accepts as input two arbitrarily nested arrays of strings.
+  It returns the sum of the number of necessary one character substitutions
+  multiplied by 2 to the power of the depth of nesting at which substitution must occur
+*/
+
 const myFunction = (arr1, arr2) => {
   let total = 0;
 
+  /* third parameter is the level of nesting.
+  It begins at -1 as initially, this function will call itself before diving deeper into the array.*/
   worldsWorstFunction(arr1, arr2, -1);
 
   return total;
 
+  /* This function uses recursion to simulatenously work through the arrays.
+  If an element is an array itself, it will call itself and dive deeper
+  into the nested array. If the arguments aren't both strings or arrays OR
+  their lengths don't match, the fail condition will be set: total = -1
+  */
   function worldsWorstFunction(arg1, arg2, nestLvl) {
-    if (checkIfBothArrays(arg1, arg2)) {
-      if (!checkLengthsMatch(arg1, arg2)) total = -1;
+    if (checkIfBothArrays(arg1, arg2) && checkLengthsMatch(arg1, arg2)) {
+      // As array nesting will deepen, increment nestLvl, ready for next call
       nestLvl++;
       for (let i = 0; i < arg1.length; i++) {
         if (total === -1) break;
         worldsWorstFunction(arg1[i], arg2[i], nestLvl);
       }
-    } else if (checkIfBothStrings(arg1, arg2)) {
-      if (!checkLengthsMatch(arg1, arg2)) total = -1;
-      else {
-        let diffCount = compareStrings(arg1, arg2);
-        total += diffCount * Math.pow(2, nestLvl);
-      }
+    } else if (checkIfBothStrings(arg1, arg2) && checkLengthsMatch(arg1, arg2)) {
+      // Count how many character substitutions are needed to match strings
+      let diffCount = compareStrings(arg1, arg2);
+      // Multiply the number of character subtitutions by 2 to the power of the depth of nesting
+      // then add to total
+      total += diffCount * Math.pow(2, nestLvl);
     } else total = -1;
   }
 };
